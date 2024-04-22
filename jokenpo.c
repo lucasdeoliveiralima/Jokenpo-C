2f08d8e6669bd17906641e5d865dcf9e57e0737c
lucasdeoliveiralima <167724471+lucasdeoliveiralima@users.noreply.github.com>
2024-04-22 17:51:57 -0300

    commit #2 - correção de bugs
    - tradução das constantes de cores
    - remoção da acentuação (compatibilidade para o Falcon c++)
    - acréscimo de espaço para o tamanho das variáveis no array de jogadas possíveis
        - de 7 para 8, para incluir o caractere de final de string \0 (estava dando problema no Falcon c++)

--- jokenpo.c
+++ jokenpo.c
@@ -6,14 +6,14 @@
 #include <locale.h>
 
 // Define as cores das fontes para o console
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define CYAN    "\x1b[36m"
+#define VERMELHO     "\x1b[31m"
+#define VERDE        "\x1b[32m"
+#define AMARELO      "\x1b[33m"
+#define AZUL         "\x1b[34m"
+#define CIANO        "\x1b[36m"
 
 // Reseta para a cor padrão
-#define RESET   "\x1b[0m"
+#define RESET        "\x1b[0m"
 
 // Jokenpo.c
 
@@ -30,12 +30,12 @@ int main(){
         int c;
         int sair;
 
-        // lista para chamar os nomes das jogadas possíveis (substitui um switch)
-        char jogadas_possiveis[3][7] = {"PEDRA", "PAPEL", "TESOURA"};
+        // lista para chamar os nomes das jogadas possiveis (substitui um switch)
+        char jogadas_possiveis[3][8] = {"PEDRA", "PAPEL", "TESOURA"};
 
         bool vitoria, derrota, empate;
 
-        // permite que a geração de números aleatórios seja diferente para cada execução do programa
+        // permite que a geracao de numeros aleatorios seja diferente para cada execucao do programa
         srand(time(NULL));
 
         printf("JokenBot: ... Outro desafiante? ... \n");
@@ -49,70 +49,70 @@ int main(){
         system("cls");
 
         printf("Hahaha! Boa sorte");
-        printf(BLUE " %s" RESET, nome_jogador);
+        printf(AZUL " %s" RESET, nome_jogador);
         printf("\nQuem marcar 5 pontos vence! >:D\n\n");
 
         while(1){
-        printf("\nEscolha a jogada");
-        printf(GREEN" vencedora\n\n"RESET);
-        printf("0 - PEDRA\n1 - PAPEL\n2 - TESOURA\n3 - ENCERRAR DUELO");
-
-        printf("\n\nSua jogada: ");
-        scanf("%d", &jogada_jogador);
-
-        // limpa o buffer de entrada, impedindo que o caractere de newline seja executado no próximo input de dados
-        while ((c = getchar()) != '\n' && c != EOF);
-
-        system("cls");
-
-        // gera uma jogada aleatória entre 0 e 2
-        jogada_computador = rand() % 3;
-        printf("\n\n===============================================================");
-        printf(BLUE"\n%s [%s]"RESET, nome_jogador, jogadas_possiveis[jogada_jogador]);
-        printf(" x ");
-        printf(CYAN"[%s] JokenBot\n\n"RESET, jogadas_possiveis[jogada_computador]);
-
-        // isola os comportamentos de vitória, empate e derrota
-        vitoria = (jogada_jogador == 0 && jogada_computador == 2)||
-                  (jogada_jogador == 1 && jogada_computador == 0)||
-                  (jogada_jogador == 2 && jogada_computador == 1);
-
-        empate = jogada_jogador == jogada_computador;
-
-        derrota = (jogada_jogador == 0 && jogada_computador == 1)||
-                  (jogada_jogador == 1 && jogada_computador == 2)||
-                  (jogada_jogador == 2 && jogada_computador == 0);
-
-        if(vitoria){
-            printf(GREEN "JokenBot: Foi um golpe de sorte! >:|" RESET);
-            placar_jogador++;
-        }
-        else if(empate){
-            printf(YELLOW "JokenBot: é... :|" RESET);
-        }
-        else if(derrota){
-            printf(RED "JokenBot: Muito fácil hahaha >:D" RESET);
-            placar_computador++;
-        } else {
-            printf("JokenBot: Esta tão nervoso que não lembra dos movimentos válidos? xD");
-        }
-
-        printf(BLUE"\n\n%s %d" RESET, nome_jogador, placar_jogador);
-        printf(" x ");
-        printf(CYAN"%d CPU\n" RESET, placar_computador);
-        printf("===============================================================\n\n");
-
-        if (placar_jogador == 5){
-            printf(GREEN"VOCÊ VENCEU!"RESET);
-            printf(BLUE"\nJokenBot: É... terei que reconhecer que você me superou :)"RESET);
-            break;
-        }
-        if (placar_computador == 5){
-            printf(RED"VOCÊ PERDEU"RESET);
-            printf(CYAN"\nJokenBot: Mais uma vítima para a conta! >:D"RESET);
-            break;
-        }
-    }
+	        printf("\nEscolha a jogada");
+	        printf(VERDE" vencedora\n\n"RESET);
+	        printf("0 - PEDRA\n1 - PAPEL\n2 - TESOURA\n");
+	
+	        printf("\n\nSua jogada: ");
+	        scanf("%d", &jogada_jogador);
+	
+	        // limpa o buffer de entrada, impedindo que o caractere de newline seja executado no proximo input de dados
+	        while ((c = getchar()) != '\n' && c != EOF);
+	
+	        system("cls");
+	
+	        // gera uma jogada aleatoria entre 0 e 2
+	        jogada_computador = rand() % 3;
+	        printf("\n\n===============================================================");
+	        printf(AZUL"\n%s [%s]"RESET, nome_jogador, jogadas_possiveis[jogada_jogador]);
+	        printf(" x ");
+	        printf(CIANO"[%s] JokenBot\n\n"RESET, jogadas_possiveis[jogada_computador]);
+	
+	        // isola os comportamentos de vitoria, empate e derrota
+	        vitoria = (jogada_jogador == 0 && jogada_computador == 2)||
+	                  (jogada_jogador == 1 && jogada_computador == 0)||
+	                  (jogada_jogador == 2 && jogada_computador == 1);
+	
+	        empate = jogada_jogador == jogada_computador;
+	
+	        derrota = (jogada_jogador == 0 && jogada_computador == 1)||
+	                  (jogada_jogador == 1 && jogada_computador == 2)||
+	                  (jogada_jogador == 2 && jogada_computador == 0);
+	
+	        if(vitoria){
+	            printf(VERDE "JokenBot: Foi um golpe de sorte! >:|" RESET);
+	            placar_jogador++;
+	        }
+	        else if(empate){
+	            printf(AMARELO "JokenBot: e... :|" RESET);
+	        }
+	        else if(derrota){
+	            printf(VERMELHO "JokenBot: Muito facil hahaha >:D" RESET);
+	            placar_computador++;
+	        } else {
+	            printf("JokenBot: Esta tao nervoso que nao lembra dos movimentos validos? xD");
+	        }
+	
+	        printf(AZUL"\n\n%s %d" RESET, nome_jogador, placar_jogador);
+	        printf(" x ");
+	        printf(CIANO"%d CPU\n" RESET, placar_computador);
+	        printf("===============================================================\n\n");
+	
+	        if (placar_jogador == 5){
+	            printf(VERDE"VOCE VENCEU!"RESET);
+	            printf(AZUL"\nJokenBot: Terei que reconhecer que voce me superou :)"RESET);
+	            break;
+	        }
+	        if (placar_computador == 5){
+	            printf(VERMELHO"VOCE PERDEU"RESET);
+	            printf(CIANO"\nJokenBot: Mais uma vitima para a conta! >:D"RESET);
+	            break;
+	        }
+	    }
 
         printf("\n\nDeseja jogar novamente? \n\n0 - NAO \nOutra tecla - SIM");
         scanf("%d", &sair);
